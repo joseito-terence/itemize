524f602a42477e4ec1a21d9bc309871c0cd8ccd8
Joseito Fernandes <joseito.terence@gmail.com>
2023-10-15 13:25:49 +0530

    merging react-navigation and rn-paper themes

--- index.js
+++ index.js
@@ -4,15 +4,37 @@
 
 import { AppRegistry } from 'react-native';
 import React from 'react';
+import { useColorScheme } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import { NavigationContainer } from '@react-navigation/native';
-import { PaperProvider } from 'react-native-paper';
+import {
+  PaperProvider,
+  adaptNavigationTheme,
+  MD3LightTheme,
+  MD3DarkTheme,
+} from 'react-native-paper';
+import {
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
+import merge from 'deepmerge';
+
+const { LightTheme, DarkTheme } = adaptNavigationTheme({
+  reactNavigationLight: NavigationDefaultTheme,
+  reactNavigationDark: NavigationDarkTheme,
+});
+
+const CombinedDefaultTheme = merge(MD3LightTheme, LightTheme);
+const CombinedDarkTheme = merge(MD3DarkTheme, DarkTheme);
 
 function Main() {
+  const isDarkMode = useColorScheme() === 'dark';
+  const theme = isDarkMode ? CombinedDarkTheme : CombinedDefaultTheme;
+
   return (
-    <PaperProvider>
-      <NavigationContainer>
+    <PaperProvider theme={theme}>
+      <NavigationContainer theme={theme}>
         <App />
       </NavigationContainer>
     </PaperProvider>

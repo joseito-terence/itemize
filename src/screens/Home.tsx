50e7e62d5e1b66010210eda77d82e56124aa5fef
Joseito Fernandes <joseito.terence@gmail.com>
2023-10-15 23:08:58 +0530

    home screen header

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,59 @@
-import { View, Text } from 'react-native';
+/* eslint-disable react-native/no-inline-styles */
+import { View, ScrollView } from 'react-native';
 import React from 'react';
+import { Text, Card, useTheme } from 'react-native-paper';
+import { Shadow } from 'react-native-shadow-2';
 
 export default function Home() {
+  const theme = useTheme();
+
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <ScrollView>
+      <View
+        className="p-4 rounded-b-2xl"
+        style={{ backgroundColor: theme.colors.secondaryContainer }}>
+        <Text variant="headlineLarge" className="font-bold mt-4 mb-6">
+          Home
+        </Text>
+
+        <View className="flex-row mx-auto mb-4" style={{ gap: 24 }}>
+          <InfoCard title="2" text="Locations" />
+          <InfoCard title="103" text="Items" />
+        </View>
+      </View>
+    </ScrollView>
   );
 }
+
+const InfoCard = ({ title = '', text = '' }) => {
+  const theme = useTheme();
+
+  return (
+    <Shadow
+      style={{ flex: 1, borderRadius: 10, aspectRatio: 1 }}
+      distance={1}
+      startColor={'#000'}
+      endColor={'#ff00ff10'}
+      offset={[-3, 3]}>
+      <Card
+        mode="elevated"
+        className="flex-1 aspect-square justify-center"
+        style={{ backgroundColor: theme.colors.primary }}>
+        <Card.Content>
+          <Text
+            variant="headlineLarge"
+            style={{ color: theme.colors.onPrimary }}
+            className="text-center font-extrabold text-5xl">
+            {title}
+          </Text>
+          <Text
+            variant="bodyLarge"
+            style={{ color: theme.colors.onPrimary }}
+            className="text-center font-bold text-2xl">
+            {text}
+          </Text>
+        </Card.Content>
+      </Card>
+    </Shadow>
+  );
+};

0c2df8c90bcab0ad598af1429aed408c502c5c8f
Joseito Fernandes <joseito.terence@gmail.com>
2023-10-16 19:34:02 +0530

    Item card colors

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,20 +1,27 @@
 import { View, Image } from 'react-native';
 import React from 'react';
-import { Card, Text } from 'react-native-paper';
+import { Card, Text, useTheme } from 'react-native-paper';
 
 export default function ItemCard() {
+  const theme = useTheme();
+
   return (
-    <Card>
+    <Card mode="contained">
       <Card.Content className="flex-row">
         <Image
           source={{ uri: 'https://picsum.photos/700' }}
           className="w-[100] h-[100] rounded-md"
         />
         <View className="ml-4">
-          <Text variant="headlineSmall" className="font-bold">
+          <Text
+            variant="headlineSmall"
+            className="font-bold"
+            style={{ color: theme.colors.onPrimaryContainer }}>
             Item Name
           </Text>
-          <Text variant="bodyMedium" className="text-gray-500">
+          <Text
+            variant="bodyMedium"
+            style={{ color: theme.colors.onPrimaryContainer }}>
             Item Description
           </Text>
         </View>

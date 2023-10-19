9748c3682af2a81c6f526fb5247b41d1f2ce8730
Joseito Fernandes <joseito.terence@gmail.com>
2023-10-19 22:28:17 +0530

    home screen header transition on scroll

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,79 +1,220 @@
 /* eslint-disable react-native/no-inline-styles */
-import { View, ScrollView } from 'react-native';
+import { Dimensions, View } from 'react-native';
 import React from 'react';
 import { Text, Card, useTheme, Button } from 'react-native-paper';
-import { Shadow } from 'react-native-shadow-2';
 import ItemCard from '../components/ItemCard';
+import Animated, {
+  Extrapolate,
+  SharedValue,
+  interpolate,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+} from 'react-native-reanimated';
+import { withAnimated } from '../hooks';
+
+const { width } = Dimensions.get('window');
+const INFO_CARD_SIZE = (width - 48) / 2;
+
+const HEADER_HEIGHT = {
+  min: 150,
+  max: 300,
+};
+const SCROLL_DISTANCE = HEADER_HEIGHT.max - HEADER_HEIGHT.min;
+
+const AnimatedText = withAnimated(Text);
 
 export default function Home() {
   const theme = useTheme();
+  const scrollY = useSharedValue(0);
+
+  const scrollHandler = useAnimatedScrollHandler(event => {
+    scrollY.value = event.contentOffset.y;
+  });
+
+  const rHeaderStyle = useAnimatedStyle(() => {
+    const height = interpolate(
+      scrollY.value,
+      [0, SCROLL_DISTANCE],
+      [HEADER_HEIGHT.max, HEADER_HEIGHT.min],
+      Extrapolate.CLAMP,
+    );
+
+    const paddingTop = interpolate(
+      scrollY.value,
+      [0, SCROLL_DISTANCE],
+      [16, 0],
+      Extrapolate.CLAMP,
+    );
+
+    return { height, paddingTop };
+  });
+
+  const rHeaderTextStyle = useAnimatedStyle(() => {
+    const fontSize = interpolate(
+      scrollY.value,
+      [0, SCROLL_DISTANCE],
+      [32, 22],
+      Extrapolate.CLAMP,
+    );
+
+    const marginBottom = interpolate(
+      scrollY.value,
+      [0, SCROLL_DISTANCE],
+      [24, 12],
+      Extrapolate.CLAMP,
+    );
+
+    return { fontSize, marginBottom };
+  });
 
   return (
-    <ScrollView>
-      <View
-        className="p-4 rounded-b-2xl"
-        style={{ backgroundColor: theme.colors.secondaryContainer }}>
-        <Text variant="displaySmall" className="font-bold mt-4 mb-6">
+    <View className="flex-1">
+      <Animated.View
+        className="rounded-b-2xl left-0 right-0"
+        style={[
+          {
+            backgroundColor: theme.colors.secondaryContainer,
+            padding: 16,
+            marginBottom: 14,
+          },
+          rHeaderStyle,
+        ]}>
+        <Animated.Text
+          style={[
+            rHeaderTextStyle,
+            { color: theme.colors.onSecondaryContainer },
+          ]}
+          className="font-bold text-[30px] mt-4 mb-6">
           Home
-        </Text>
+        </Animated.Text>
 
-        <View className="flex-row mx-auto mb-4" style={{ gap: 24 }}>
-          <InfoCard title="2" text="Locations" />
-          <InfoCard title="103" text="Items" />
+        <View className="flex-1 flex-row mx-auto mb-4" style={{ gap: 20 }}>
+          <InfoCard title="2" text="Locations" scrollY={scrollY} />
+          <InfoCard title="103" text="Items" scrollY={scrollY} />
         </View>
-      </View>
+      </Animated.View>
+      <Animated.ScrollView onScroll={scrollHandler}>
+        <View className="p-4">
+          <View className="flex-row justify-between items-center mb-4">
+            <Text variant="headlineMedium" className="font-bold">
+              Recents
+            </Text>
+            <Button>View All</Button>
+          </View>
 
-      <View className="p-4">
-        <View className="flex-row justify-between items-center mb-4">
-          <Text variant="headlineMedium" className="font-bold">
-            Recents
-          </Text>
-          <Button>View All</Button>
+          <View style={{ gap: 14 }}>
+            {Array.from({ length: 10 }).map((_, i) => (
+              <ItemCard
+                key={i}
+                id={i.toString()}
+                sharedTransitionTag={`Home_${i.toString()}`}
+              />
+            ))}
+          </View>
         </View>
-
-        <View style={{ gap: 14 }}>
-          {Array.from({ length: 10 }).map((_, i) => (
-            <ItemCard
-              key={i}
-              id={i.toString()}
-              sharedTransitionTag={`Home_${i.toString()}`}
-            />
-          ))}
-        </View>
-      </View>
-    </ScrollView>
+      </Animated.ScrollView>
+    </View>
   );
 }
 
-const InfoCard = ({ title = '', text = '' }) => {
+type InfoCardProps = {
+  title: string;
+  text: string;
+  scrollY: SharedValue<number>;
+};
+
+const InfoCard = ({ title, text, scrollY }: InfoCardProps) => {
   const theme = useTheme();
 
+  const rCardStyle = useAnimatedStyle(() => {
+    const height = interpolate(
+      scrollY.value,
+      [0, SCROLL_DISTANCE],
+      [INFO_CARD_SIZE, HEADER_HEIGHT.min - 80],
+      Extrapolate.CLAMP,
+    );
+
+    return { height };
+  });
+
+  const rTitleStyle = useAnimatedStyle(() => {
+    const fontSize = interpolate(
+      scrollY.value,
+      [0, SCROLL_DISTANCE],
+      [32, 22],
+      Extrapolate.CLAMP,
+    );
+
+    const translateX = interpolate(
+      scrollY.value,
+      [0, SCROLL_DISTANCE],
+      [0, -50],
+      Extrapolate.CLAMP,
+    );
+
+    const translateY = interpolate(
+      scrollY.value,
+      [0, SCROLL_DISTANCE],
+      [0, -6],
+      Extrapolate.CLAMP,
+    );
+
+    return { fontSize, transform: [{ translateX }, { translateY }] };
+  });
+
+  const rTextStyle = useAnimatedStyle(() => {
+    const fontSize = interpolate(
+      scrollY.value,
+      [0, SCROLL_DISTANCE],
+      [24, 18],
+      Extrapolate.CLAMP,
+    );
+
+    const translateX = interpolate(
+      scrollY.value,
+      [0, SCROLL_DISTANCE],
+      [0, 10],
+      Extrapolate.CLAMP,
+    );
+
+    const translateY = interpolate(
+      scrollY.value,
+      [0, SCROLL_DISTANCE],
+      [0, -46],
+      Extrapolate.CLAMP,
+    );
+
+    return { fontSize, transform: [{ translateX }, { translateY }] };
+  });
+
   return (
-    <Shadow
-      style={{ flex: 1, borderRadius: 10, aspectRatio: 1 }}
-      distance={1}
-      startColor={'#000'}
-      endColor={'#ff00ff10'}
-      offset={[-3, 3]}>
+    <Animated.View style={[{ width: INFO_CARD_SIZE }, rCardStyle]}>
       <Card
         mode="elevated"
-        className="flex-1 aspect-square justify-center"
-        style={{ backgroundColor: theme.colors.primary }}>
+        className="flex-1 justify-center"
+        elevation={5}
+        style={{
+          backgroundColor: theme.colors.primary,
+          width: INFO_CARD_SIZE,
+          borderColor: 'black',
+          borderWidth: 2,
+        }}>
         <Card.Content>
-          <Text
+          <AnimatedText
             variant="headlineLarge"
-            style={{ color: theme.colors.onPrimary }}
+            style={[rTitleStyle, { color: theme.colors.onPrimary }]}
             className="text-center font-extrabold text-5xl">
             {title}
-          </Text>
-          <Text
+          </AnimatedText>
+          <AnimatedText
             variant="bodyLarge"
-            style={{ color: theme.colors.onPrimary }}
+            style={[rTextStyle, { color: theme.colors.onPrimary }]}
             className="text-center font-bold text-2xl">
             {text}
-          </Text>
+          </AnimatedText>
         </Card.Content>
       </Card>
-    </Shadow>
+    </Animated.View>
   );
 };

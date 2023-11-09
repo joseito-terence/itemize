ca956d9c1776dbf55f35c6237554233d03af8a7e
Joseito Fernandes <joseito.terence@gmail.com>
2023-11-09 17:48:53 +0530

    Refactor imports and remove unused code

--- App.tsx
+++ App.tsx
@@ -6,8 +6,7 @@
  */
 import 'react-native-gesture-handler';
 import React from 'react';
-import { useColorScheme, View } from 'react-native';
-import { Colors } from 'react-native/Libraries/NewAppScreen';
+import { View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import type { BottomTabsParamList, RootStackParamList } from './types';
 import { createMaterialBottomTabNavigator } from 'react-native-paper/react-navigation';
@@ -31,14 +30,9 @@ const Tab = createMaterialBottomTabNavigator<BottomTabsParamList>();
 
 function App(): JSX.Element {
   const user = useAuthUser();
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : '#fff',
-  };
 
   return (
-    <View style={backgroundStyle} className="flex-1">
+    <View className="flex-1">
       {!!user && <DataFetcher />}
       <ColorSchemeProvider>
         <Stack.Navigator screenOptions={{ headerShown: false }}>

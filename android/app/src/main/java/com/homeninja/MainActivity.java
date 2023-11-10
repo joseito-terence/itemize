ac2a9bb8ae235615b651150fe713244f3235ca7b
Joseito Fernandes <joseito.terence@gmail.com>
2023-11-10 19:37:24 +0530

    added: splash screen

--- android/app/src/main/java/com/homeninja/MainActivity.java
+++ android/app/src/main/java/com/homeninja/MainActivity.java
@@ -1,5 +1,6 @@
 package com.homeninja;
-
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
@@ -28,5 +29,11 @@ public class MainActivity extends ReactActivity {
         getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
         DefaultNewArchitectureEntryPoint.getFabricEnabled());
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      SplashScreen.show(this);  // here
+      super.onCreate(savedInstanceState);
   }
 }
--- index.js
+++ index.js
@@ -4,7 +4,7 @@
  */
 
 import { AppRegistry } from 'react-native';
-import React from 'react';
+import React, { useEffect } from 'react';
 import App from './App';
 import { name as appName } from './app.json';
 import { NavigationContainer } from '@react-navigation/native';
@@ -23,6 +23,7 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { Provider } from 'react-redux';
 import { store } from './src/redux/store';
 import { useAppSelector } from './src/redux/hooks';
+import SplashScreen from 'react-native-splash-screen';
 
 const { LightTheme, DarkTheme } = adaptNavigationTheme({
   reactNavigationLight: NavigationDefaultTheme,
@@ -33,6 +34,10 @@ const CombinedDefaultTheme = merge(MD3LightTheme, LightTheme);
 const CombinedDarkTheme = merge(MD3DarkTheme, DarkTheme);
 
 function Main() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
   return (
     <Provider store={store}>
       <Providers>

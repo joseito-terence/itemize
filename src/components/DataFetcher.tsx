495a179b8afa811f0a54016724ecb8c902488e89
Joseito Fernandes <joseito.terence@gmail.com>
2023-11-10 18:15:16 +0530

    Refactor data fetching code to only fetch data for the current user.

--- src/components/DataFetcher.tsx
+++ src/components/DataFetcher.tsx
@@ -10,42 +10,45 @@ export default function DataFetcher() {
   const dispatch = useAppDispatch();
 
   useEffect(() => {
-    const subscriber = firestore()
-      .collection('storages')
-      .where('userId', '==', user?.uid)
-      .onSnapshot(querySnapshot => {
-        const _storages: any = [];
-        querySnapshot.forEach(documentSnapshot => {
-          _storages.push({
-            ...documentSnapshot.data(),
-            location: documentSnapshot.data().location.toJSON(),
-            id: documentSnapshot.id,
+    if (user?.uid) {
+      const subscriber = firestore()
+        .collection('storages')
+        .where('userId', '==', user?.uid)
+        .onSnapshot(querySnapshot => {
+          const _storages: any = [];
+          querySnapshot.forEach(documentSnapshot => {
+            _storages.push({
+              ...documentSnapshot.data(),
+              location: documentSnapshot.data().location.toJSON(),
+              id: documentSnapshot.id,
+            });
           });
+
+          dispatch(setStorages(_storages));
         });
 
-        dispatch(setStorages(_storages));
-      });
-
-    return () => subscriber();
+      return () => subscriber();
+    }
   }, [dispatch, user?.uid]);
 
   useEffect(() => {
-    const subscriber = firestore()
-      .collection('items')
-      .where('userId', '==', user?.uid)
-      .onSnapshot(querySnapshot => {
-        const _items: any = [];
-        querySnapshot.forEach(documentSnapshot => {
-          _items.push({
-            ...documentSnapshot.data(),
-            id: documentSnapshot.id,
+    if (user?.uid) {
+      const subscriber = firestore()
+        .collection('items')
+        .where('userId', '==', user?.uid)
+        .onSnapshot(querySnapshot => {
+          const _items: any = [];
+          querySnapshot.forEach(documentSnapshot => {
+            _items.push({
+              ...documentSnapshot.data(),
+              id: documentSnapshot.id,
+            });
           });
+
+          dispatch(setItems(_items));
         });
-
-        dispatch(setItems(_items));
-      });
-
-    return () => subscriber();
+      return () => subscriber();
+    }
   }, [dispatch, user?.uid]);
 
   return <></>;

ee86ab73381b07cfe9a7d8753e72dfa09488b8dd
Joseito Fernandes <joseito.terence@gmail.com>
2023-11-16 09:41:47 +0530

    Update AddInvoiceActionSheet component with new buttons.

--- src/components/AddInvoiceActionSheet.tsx
+++ src/components/AddInvoiceActionSheet.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View } from 'react-native';
-import { Portal, Modal, Button, useTheme } from 'react-native-paper';
+import { Portal, Modal, IconButton, useTheme, Text } from 'react-native-paper';
 import { useDisclose } from '../hooks';
 
 interface Props extends ReturnType<typeof useDisclose> {}
@@ -18,16 +18,28 @@ export default function AddInvoiceActionSheet({ ...props }: Props) {
         dismissable
         dismissableBackButton>
         <View
-          className="w-full px-4 py-6 rounded-t-xl"
+          className="w-full px-4 py-6 rounded-t-xl flex-row justify-evenly"
           style={{ backgroundColor: theme.colors.inverseOnSurface }}>
-          <Button icon="camera" mode="outlined" className="mb-4">
-            Image
-          </Button>
-          <Button icon="file-pdf-box" mode="outlined">
-            PDF
-          </Button>
+          <Button icon="camera" text="Camera" />
+          <Button icon="file-image" text="Gallery" />
+          <Button icon="file-pdf-box" text="PDF" />
         </View>
       </Modal>
     </Portal>
   );
 }
+
+interface ButtonProps {
+  icon: string;
+  text: string;
+  onPress?: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ icon, text, onPress }) => {
+  return (
+    <View className="items-center">
+      <IconButton icon={icon} mode="outlined" size={40} onPress={onPress} />
+      <Text>{text}</Text>
+    </View>
+  );
+};
